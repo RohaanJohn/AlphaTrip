df3d1dc9d107764755d9964b4436ed26b144a603
Rohaan John <rohaanrenujohn@gmail.com>
2021-09-25 12:55:45 +0530

    Update settings.py

--- telusko/settings.py
+++ telusko/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'telusko.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'dd3idi1j3vga1e',
         'USER': 'xryvngwfkuvvou',
         'PASSWORD': '093e3fe3ec1ba685b9746860490fbd0ddf9fe4907104b57e1ccc3198ce08576b',
