f7b18f19f1f83172440c43eab9af997443ddb900
Rohaan John <rohaanrenujohn@gmail.com>
2021-09-29 19:23:24 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -61,28 +61,28 @@ def contact(request):
         email = request.POST['email']
         username = request.POST['username']
         msg = request.POST['message']
-        Email_Password = os.environ.get('Email_Password')
+        
 
          # get email and password from environment variables
     
-    EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+        EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
     
     # set up email content
-    msg = EmailMessage()
-    msg['Subject'] = 'Alpha Trip'
-    msg['From'] = thealphadebuggers@gmail.com
-    msg['To'] = email
-    msg.set_content(f"Hi {username}! We will look into your message and send you a reply as soon as possible if needed. Thank you for using Alpha Trip!")
+        msg = EmailMessage()
+        msg['Subject'] = 'Alpha Trip'
+        msg['From'] = thealphadebuggers@gmail.com
+        msg['To'] = email
+        msg.set_content(f"Hi {username}! We will look into your message and send you a reply as soon as possible if needed. Thank you for using Alpha Trip! Your message: {msg}")
     
     # send email
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-        s.login(thealphadebuggers@gmail.com, EMAIL_PASSWORD)
-        s.send_message(msg)
-        s.quit()
+         with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+            s.login(thealphadebuggers@gmail.com, EMAIL_PASSWORD)
+            s.send_message(msg)
+            s.quit()
         
 
       
-        return redirect('/')
+    return redirect('/')
   else:
         return render(request,'contact.html')
         return redirect('contact')
