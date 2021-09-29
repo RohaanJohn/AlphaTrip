0ba02b6ec261ac62d3debbef45de975f381392ca
Rohaan John <rohaanrenujohn@gmail.com>
2021-09-29 19:49:18 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 import smtplib
+import os
 from os import environ
 # Create your views here.
 
