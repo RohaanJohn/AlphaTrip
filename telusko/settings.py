fa4ea369550489d7f89f58a22bda2e723b590ffa
Rohaan John <rohaanrenujohn@gmail.com>
2021-10-15 08:49:49 +0530

    Update settings.py

--- telusko/settings.py
+++ telusko/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import django_heroku
+from os import environ
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
