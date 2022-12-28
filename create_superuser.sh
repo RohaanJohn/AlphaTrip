f90626a7d2564774b65c81b0f769e2f76f1573bd
Rohaan John <rohaanrenujohn@gmail.com>
2022-12-28 11:03:09 +0530

    Update create_superuser.sh

--- create_superuser.sh
+++ create_superuser.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python manage.py createsuperuser --'Rohaan' superuser --'1234' password --noinput
+python manage.py createsuperuser --username superuser --email superuser@example.com --password password --noinput
