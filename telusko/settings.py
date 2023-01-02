5d855926564ca84a0263f3baf6042c2641c74584
Rohaan John <rohaanrenujohn@gmail.com>
2023-01-02 18:59:08 +0530

    Update settings.py

--- telusko/settings.py
+++ telusko/settings.py
@@ -29,6 +29,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['alphatrip.up.railway.app']
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'thealphadummy@gmail.com'
+EMAIL_HOST_PASSWORD = 'alphadummy12345689'
 # Application definition
 
 INSTALLED_APPS = [
